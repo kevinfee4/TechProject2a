d4ab45914e5605878c7e88ea1735660de3d37b01
Kevin <Kevin@DESKTOP-976O6J1>
2020-09-14 18:14:11 -0400

    techproj2a

--- TechProject2a/Program.cs
+++ TechProject2a/Program.cs
@@ -6,7 +6,39 @@ namespace TechProject2a
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Enter an Integer value between 1 and 25 to execute an iterative statement: ");
+
+			try
+			{
+				string input = Console.ReadLine();
+				int value_of_input = int.Parse(input);
+
+				if ((value_of_input > 0) && (value_of_input <= 25))
+				{
+					Console.WriteLine("Executing a For Loop!");
+					Console.WriteLine("The For Loop will iterate " + value_of_input.ToString() + " times.");
+
+					for (int i = 0; i < value_of_input; i++)
+					{
+						Console.WriteLine("The value of the variable i in this iteration is: " + i.ToString());
+					}
+					Console.WriteLine("Press any key to exit the program...");
+					Console.ReadKey(true);
+				}
+
+				else
+				{
+					Console.WriteLine("Please enter an integer between 1 and 25 and try again...");
+					Console.WriteLine("Press any key to exit the program and try again...");
+					Console.ReadKey(true);
+				}
+			}
+			catch
+			{
+				Console.WriteLine("Please enter an integer value and try running the program again...");
+				Console.WriteLine("Press any key to exit the program...");
+				Console.ReadKey(true);
+			}
 		}
 	}
 }
